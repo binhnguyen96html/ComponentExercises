46937ed5061bfdfa7f4ccbc8d32f65c98a404508
binhnguyen96html <107281236+binhnguyen96html@users.noreply.github.com>
2023-02-21 23:29:39 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import ShowName from "./components/ShowName";
+import ShowName2 from './components/ShowName2';
+import AddComponent from './components/AddComponent';
+import Alert from './components/Alert';
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <div>
+      <ShowName color="red" fontSize="20px" name="Bonnie" />
+      <ShowName color="blue" fontSize="25px"/>
+
+      <ShowName2 name="Binh" color='purple'/>
+      <ShowName2 name="Bonnie" fontSize ="25px"/>
+
+      <AddComponent firstNum={1} secondNum={2}/>
+
+      <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." />
+
+    </div>
+  );
+}
+
+export default App;
